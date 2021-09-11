ffdafde6b7b14872edcca2260e1cf1db1b3ee4bc
rolandopalermo <support@rolandopalermo.com>
2021-09-11 12:08:02 -0500

    Enhance messages

--- src/main/java/com/rolandopalermo/facturacion/ec/common/exception/ResourceNotFoundException.java
+++ src/main/java/com/rolandopalermo/facturacion/ec/common/exception/ResourceNotFoundException.java
@@ -1,7 +1,5 @@
 package com.rolandopalermo.facturacion.ec.common.exception;
 
-import java.text.MessageFormat;
-
 /**
  * Clase para manejar excepciones lanzadas por registros no encontrados
  *
@@ -9,10 +7,8 @@ import java.text.MessageFormat;
  */
 public class ResourceNotFoundException extends VeronicaException {
 
-    private static final String MESSAGE = "El recurso [{0}] no existe o su estatus es inválido para esta acción";
-
-    public ResourceNotFoundException(final String resourceId) {
-        super(MessageFormat.format(MESSAGE, resourceId));
+    public ResourceNotFoundException(final String message) {
+        super(message);
     }
 
 }
