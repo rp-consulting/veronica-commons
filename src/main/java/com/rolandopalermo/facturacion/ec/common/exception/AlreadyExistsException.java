010c7113d249a2c20a8f9abc0e99e8f99c6df0da
rolandopalermo <support@rolandopalermo.com>
2021-09-11 16:04:21 -0500

    Enhance exception messages

--- src/main/java/com/rolandopalermo/facturacion/ec/common/exception/AlreadyExistsException.java
+++ src/main/java/com/rolandopalermo/facturacion/ec/common/exception/AlreadyExistsException.java
@@ -1,13 +1,9 @@
 package com.rolandopalermo.facturacion.ec.common.exception;
 
-import java.text.MessageFormat;
-
 public class AlreadyExistsException extends VeronicaException {
 
-    private static final String MESSAGE = "El recurso [{0}] ya se encuentra registrado";
-
-    public AlreadyExistsException(final String resourceId) {
-        super(MessageFormat.format(MESSAGE, resourceId));
+    public AlreadyExistsException(final String message) {
+        super(message);
     }
 
 }
